b91c631763c27189fb1971b01a4d3a1db736246b
ramos <ramosfranciscotch@gmail.com>
2026-07-21 23:57:35 +0100

    fix: modal confirmação aprovação massa e ajustes carteira PDF

--- rh/views.py
+++ rh/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from django.core.cache import cache
